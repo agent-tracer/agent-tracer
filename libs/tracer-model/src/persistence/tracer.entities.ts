06a2b4dbf25e05fe7c968fec56f086edacce634f
belljun3395 <195850@jnu.ac.kr>
2026-07-29 14:50:51 +0900

    feat(tracer-model): 데이터 소스와 저장소 배선을 세운다

---
+++ libs/tracer-model/src/persistence/tracer.entities.ts
@@ -0,0 +1,46 @@
+import { TaskCleanupSuggestionEntity } from "../cleanup/task.cleanup.suggestion.entity.js";
+import { AgentEventView } from "../timeline/event/agent.event.view.js";
+import { AgentRuleView } from "../rule/agent.rule.view.js";
+import { AgentTaskView } from "../task/agent.task.view.js";
+import { DaemonHealthEntity } from "../daemon/daemon.health.entity.js";
+import { MemoEntity } from "../memo/memo.entity.js";
+import { RecipeApplicationEntity } from "../recipe/application/recipe.application.entity.js";
+import { RecipeEntity } from "../recipe/recipe.entity.js";
+import { RuleEntity } from "../rule/rule.entity.js";
+import { VerdictEntity } from "../rule/verification/verdict.entity.js";
+import { SearchOutboxEntity } from "../search/search.outbox.entity.js";
+import { AppSettingEntity } from "../settings/app.setting.entity.js";
+import { SessionEntity } from "../session/session.entity.js";
+import { TaskEntity } from "../task/task.entity.js";
+import { TaskUserStateEntity } from "../task/user-state/task.user.state.entity.js";
+import { TagEntity } from "../tag/tag.entity.js";
+import { TaskTagEntity } from "../tag/task-tag.entity.js";
+import { EventEntity } from "../timeline/event/event.entity.js";
+import { TurnEntity } from "../timeline/turn/turn.entity.js";
+import { AgentTurnView } from "../timeline/turn/agent.turn.view.js";
+import { UserEntity } from "../user/user.entity.js";
+
+// DataSource 등록용 전체 엔티티 목록.
+export const TRACER_ENTITIES = [
+    TaskEntity,
+    TaskUserStateEntity,
+    SessionEntity,
+    EventEntity,
+    TurnEntity,
+    RuleEntity,
+    VerdictEntity,
+    RecipeEntity,
+    RecipeApplicationEntity,
+    TaskCleanupSuggestionEntity,
+    UserEntity,
+    AppSettingEntity,
+    SearchOutboxEntity,
+    DaemonHealthEntity,
+    MemoEntity,
+    TagEntity,
+    TaskTagEntity,
+    AgentTaskView,
+    AgentEventView,
+    AgentTurnView,
+    AgentRuleView,
+];
---
+++ libs/tracer-model/src/persistence/transaction.runner.ts
@@ -0,0 +1,55 @@
+import type { DataSource, EntityManager } from "typeorm";
+import { TaskCleanupSuggestionEntity } from "../cleanup/task.cleanup.suggestion.entity.js";
+import { TaskCleanupSuggestionRepository } from "../cleanup/task.cleanup.suggestion.repository.js";
+import { MemoEntity } from "../memo/memo.entity.js";
+import { MemoRepository } from "../memo/memo.repository.js";
+import { TagEntity } from "../tag/tag.entity.js";
+import { TagRepository } from "../tag/tag.repository.js";
+import { TaskTagEntity } from "../tag/task-tag.entity.js";
+import { TaskTagRepository } from "../tag/task-tag.repository.js";
+import { RecipeEntity } from "../recipe/recipe.entity.js";
+import { RecipeRepository } from "../recipe/recipe.repository.js";
+import { RuleEntity } from "../rule/rule.entity.js";
+import { RuleRepository } from "../rule/rule.repository.js";
+import { SearchOutboxEntity } from "../search/search.outbox.entity.js";
+import { SearchOutboxRepository } from "../search/search.outbox.repository.js";
+import { TaskEntity } from "../task/task.entity.js";
+import { TaskRepository } from "../task/task.repository.js";
+import { TaskUserStateEntity } from "../task/user-state/task.user.state.entity.js";
+import { TaskUserStateRepository } from "../task/user-state/task.user.state.repository.js";
+
+// 한 트랜잭션 안에서만 유효한 저장소 묶음이며, 잡 종결과 그 부수효과를 한 커밋으로 묶어 전이 경합에서 진 실행자가 부수효과를 남기지 않게 한다.
+export interface TracerTx {
+    readonly recipes: RecipeRepository;
+    readonly rules: RuleRepository;
+    readonly cleanupSuggestions: TaskCleanupSuggestionRepository;
+    readonly tasks: TaskRepository;
+    readonly taskUserStates: TaskUserStateRepository;
+    readonly searchOutbox: SearchOutboxRepository;
+    readonly memos: MemoRepository;
+    readonly tags: TagRepository;
+    readonly taskTags: TaskTagRepository;
+}
+
+function bind(manager: EntityManager): TracerTx {
+    return {
+        recipes: new RecipeRepository(manager.getRepository(RecipeEntity)),
+        rules: new RuleRepository(manager.getRepository(RuleEntity)),
+        cleanupSuggestions: new TaskCleanupSuggestionRepository(manager.getRepository(TaskCleanupSuggestionEntity)),
+        tasks: new TaskRepository(manager.getRepository(TaskEntity)),
+        taskUserStates: new TaskUserStateRepository(manager.getRepository(TaskUserStateEntity)),
+        searchOutbox: new SearchOutboxRepository(manager.getRepository(SearchOutboxEntity)),
+        memos: new MemoRepository(manager.getRepository(MemoEntity)),
+        tags: new TagRepository(manager.getRepository(TagEntity)),
+        taskTags: new TaskTagRepository(manager.getRepository(TaskTagEntity)),
+    };
+}
+
+// 저장소가 생성자로 Repository를 받으므로 트랜잭션 매니저의 Repository로 같은 클래스를 다시 만들며, 암묵 전파 없이 어떤 쓰기가 트랜잭션 안인지 호출부에서 그대로 보인다.
+export class TransactionRunner {
+    constructor(private readonly dataSource: DataSource) {}
+
+    async run<T>(work: (tx: TracerTx) => Promise<T>): Promise<T> {
+        return this.dataSource.transaction((manager) => work(bind(manager)));
+    }
+}
