aa3e5b6e2c5bf673efa652eaf35408657dd0f45a
belljun3395 <195850@jnu.ac.kr>
2026-07-29 14:11:12 +0900

    feat(platform): 공개 표면을 배럴로 모은다

---
+++ libs/platform/src/index.ts
@@ -0,0 +1,23 @@
+export * from "./primitives/domain.error.js";
+export * from "./primitives/ulid.js";
+export * from "./primitives/clock.js";
+export * from "./primitives/deadline.js";
+export * from "./config/secret.js";
+export { loadApplicationConfig } from "./config/application.config.loader.js";
+export {
+    applicationConfigSchema,
+    type ApplicationConfig,
+    type DbConfig,
+} from "./config/application.config.schema.js";
+export * from "./auth/auth.token.js";
+export * from "./auth/execution.scope.token.js";
+export * from "./auth/cookie.js";
+export * from "./auth/rate.limiter.js";
+export * from "./db/datasource.factory.js";
+export * from "./db/schema.version.guard.js";
+export * from "./db/schema.version.assert.js";
+export * from "./kafka/kafka.types.js";
+export * from "./kafka/kafka.factory.js";
+export * from "./kafka/readiness.probe.js";
+export * from "./kafka/notification.producer.js";
+export * from "./opensearch/opensearch.factory.js";
